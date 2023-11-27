01940650ef3cb022e8839de9fc9e722923ec7164
Mike Porter <mike@graphyapp.com>
2023-11-27 00:10:30 +0000

    setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="pynanodlpapi",
     packages=["pynanodlpapi"],
-    version="0.0.1",
+    version="0.0.2",
     description="An asynchronous Python library for communicating with the NanoDLP API",
     author="Mike Porter",
     author_email="mikeporterdev@users.noreply.github.com",
